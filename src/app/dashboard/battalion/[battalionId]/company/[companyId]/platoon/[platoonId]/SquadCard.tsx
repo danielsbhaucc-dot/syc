0ffa7392d023be92284ceb4ef305cad9eed9aa9d
Daniel Rahman <Danielsbhaucc@gmail.com>
2026-01-13 20:49:28 +0000

    מה השלב הבא?

--- src/app/dashboard/battalion/[battalionId]/company/[companyId]/platoon/[platoonId]/SquadCard.tsx
+++ src/app/dashboard/battalion/[battalionId]/company/[companyId]/platoon/[platoonId]/SquadCard.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from 'react';
 import { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { SoldierCard } from './SoldierCard';
